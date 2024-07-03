5ee7fd1dc26c656c9cafbb765a6e6bfe24546f7c
KarthiKbot <102286590+KarthiKbot@users.noreply.github.com>
2024-07-03 15:42:20 +0530

    Update Program_4.c

--- Program_4.c
+++ Program_4.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
-int main() {
-    int ch;
-    float a[2];  
-    printf("Enter choice (1 for circle area, 2 for rectangle area): ");
-    scanf("%d", &ch);
-    switch(ch) {
-        case 1:
-            printf("Enter radius 'r': ");
-            scanf("%f", &a[0]);
-           
-            printf("Area of circle = %.2f\n", 3.14159 * a[0] * a[0]);
-            break;
-        case 2:
-            printf("Enter length 'l' and breadth 'b': ");
-            scanf("%f %f", &a[0], &a[1]);
-            printf("Area of rectangle = %.2f\n", a[0] * a[1]);
-            break;
-        default:
-            printf("Invalid choice\n");
+int fibonacci(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
     }
+    
+    int a = 1, b = 1, c;
+    
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    
+    return b;
+}
+
+int main() {
+    int n;
+    
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    
+    if (n <= 0) {
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 1; // Return non-zero to indicate error
+    }
+    
+    int result = fibonacci(n);
+    
+    printf("The %d-th Fibonacci number is: %d\n", n, result);
+    
     return 0;
 }
